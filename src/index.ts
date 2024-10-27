c511e953455955b842ee8b5f55afe85ea18a74c9
FelipeGomeees <felipegomes.dsn@gmail.com>
2024-10-27 19:26:04 -0300

    feat: funcao para colorir tile teste de funcao para colorir um tile, vai ser refatorado e organizado nos proximos commits

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,25 @@ import { testMap } from "./maps";
 window.onload = () => {
     const wrapper = document.getElementById("wrapper");
     if (wrapper) {
-        for (let i = 0; i < 20; i++) {
+        let x = 0;
+        let y = 0
+        for (let i = 0; i < (18 * 18); i++) {
             const div = document.createElement("div");
+            div.id = `tile-${x}-${y}`;
+            div.addEventListener("click", (e) => {
+                if (e.target) {
+                    const selectedId = (e.target as HTMLInputElement).id;
+                    map.paintColor('#33f', Number(selectedId.split("-")[1]), Number(selectedId.split("-")[2]));
+                }
+            })
+            x++;
+            if (x === 18) {
+                y++;
+                x = 0;
+            }
             wrapper.appendChild(div);
         }
     }
-    const map = new GameMap(testMap, testMap);
+    const map = new GameMap(undefined, testMap);
     map.draw();
 }   
