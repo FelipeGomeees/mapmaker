5ddf2f967f6ff0073dbb3ad6ab38382792772670
FelipeGomeees <felipegomes.dsn@gmail.com>
2024-10-31 08:21:48 -0300

    feat: texturelist atualizado

--- src/utils/lists.ts
+++ src/utils/lists.ts
@@ -1,11 +1,11 @@
 interface idToString {
     [key: number]: string;
 }
-export const detailList: idToString = {
+export const textureList: idToString = {
     0: '',
     1: 'src/assets/tiles/grass.png',
     2: 'src/assets/tiles/waves.png',
     3: 'src/assets/tiles/waterfall.png',
     4: 'src/assets/tiles/rocky.png',
-    5: 'src/assets/tiles/sandy.png',
+    5: 'src/assets/tiles/cliff.png',
 }
