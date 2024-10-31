8587ab1788cf5acd0c5a65121229e3f8c0763860
FelipeGomeees <felipegomes.dsn@gmail.com>
2024-10-31 08:22:08 -0300

    feat: inicializacao alterada atualizacao drastica na inicializacao

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import { GameMap } from "./classes/GameMap";
 import { MapWrapper } from "./classes/MapWrapper";
+import { EditorGui } from "./classes/EditorGui";
 import { testMap } from "./maps";
 
 window.onload = () => {
-    const map = new GameMap(undefined, testMap);
-    map.draw();
+    const colorCanvas = document.getElementById("color__map") as HTMLCanvasElement;
+    const ColorMap = new GameMap(colorCanvas);
+    const textureCanvas = document.getElementById("texture__map") as HTMLCanvasElement;
+    const TextureMap = new GameMap(textureCanvas);
+    ColorMap.draw();
+    TextureMap.draw();
+
     const wrapperEl = document.getElementById('wrapper');
     if (wrapperEl) {
-        const wrapper = new MapWrapper(wrapperEl, map);
+        const wrapper = new MapWrapper(wrapperEl, ColorMap);
+
+        const colorArea = document.getElementById('tile-area__color')
+        const textureArea = document.getElementById('tile-area__texture')
+        if (colorArea && textureArea && wrapper) {
+            const gui = new EditorGui(colorArea, textureArea, ColorMap, TextureMap, wrapper);
+        }
     }
 }   
