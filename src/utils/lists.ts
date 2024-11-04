cb439ba653108e4a6d621292fb901974c11db5b0
FelipeGomeees <felipegomes.dsn@gmail.com>
2024-11-03 22:00:46 -0300

    feat: lista atualizada

--- src/utils/lists.ts
+++ src/utils/lists.ts
@@ -3,9 +3,18 @@ interface idToString {
 }
 export const textureList: idToString = {
     0: '',
-    1: 'src/assets/tiles/grass.png',
-    2: 'src/assets/tiles/waves.png',
-    3: 'src/assets/tiles/waterfall.png',
-    4: 'src/assets/tiles/rocky.png',
-    5: 'src/assets/tiles/cliff.png',
+    1: 'src/assets/textures/grass.png',
+    2: 'src/assets/textures/waves.png',
+    3: 'src/assets/textures/waterfall.png',
+    4: 'src/assets/textures/rocky.png',
+    5: 'src/assets/textures/cliff.png',
+}
+export const spriteList: idToString = {
+    0: '',
+    1: 'src/assets/sprites/daisy.png',
+    2: 'src/assets/sprites/pine.png',
+    3: 'src/assets/sprites/pine_l.png',
+    4: 'src/assets/sprites/pine_r.png',
+    5: 'src/assets/sprites/pine_top.png',
+    6: 'src/assets/sprites/pine_bottom.png',
 }
